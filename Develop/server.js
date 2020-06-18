7c06f504a73aa66158e8322a2769a9f6264b265f
Linnea <linneagear@gmail.com>
2020-06-17 17:48:30 -0700

    Adding process.env.PORT to deploy locally

--- Develop/server.js
+++ Develop/server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing and static
 app.use(express.static('public'));
